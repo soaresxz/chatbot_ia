0c2b6b1f947c93e99cb318983b52e0e612022031
Antony Gabriel Soares Santana <antonygabriel042006@gmail.com>
2026-03-01 00:23:33 -0300

    fix: use python start script to handle PORT correctly

--- start.py
+++ start.py
@@ -2,5 +2,6 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))
+    print(f"🚀 Iniciando na porta {port}")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, log_level="info")
